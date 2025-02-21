7f3629c92a400c9188bd5dee65209792c0644960
“Jonathan <jonathan.rees@publicisemil.com>
2025-02-21 13:14:26 +0000

    Fix type issue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 import Todos from './components/Todos.vue'
 
 import SiteFooter from './components/SiteFooter.vue'
@@ -11,7 +11,7 @@ import SiteHeader from './components/SiteHeader.vue'
   <div class="app grow-1">
     <router-view v-slot="{ Component, route }">
       <!-- Use a custom transition or fallback to `fade` -->
-      <transition :name="route.meta.transition || 'slide-fade'">
+      <transition :name="String(route.meta.transition) || 'slide-fade'">
         <component :is="Component" />
       </transition>
     </router-view>
